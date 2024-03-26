e7ff4dac674c544da350fd3e375407ed8b1b0fd6
SnehaKhunt <snehakhunt4564@gmail.com>
2024-03-26 10:49:50 +0530

    SK | Removed unwanted code

--- utils/formikValidation.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup'
-
-export const REGISTRATION_SCHEMA = yup.object({
-    firstName: yup.string().required("Please enter first name"),
-    lastName: yup.string().required("Please enter last name"),
-    email: yup.string().email('Invalid email address').required('Please enter email'),
-    password: yup.string().min(6, 'Password must be at least 6 characters').required('Required')
-})
