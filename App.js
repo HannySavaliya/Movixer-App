3870a3dd3a8e99b1a10f65238bf17d6aa7e9d223
HannySavaliya <hannysavaliya1102@gmail.com>
2024-03-26 10:21:53 +0530

    HS | removed unwanted code

--- App.js
+++ App.js
@@ -1,79 +1,12 @@
-import 'react-native-gesture-handler';
+import React from "react";
+import { Text, View } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { AntDesign, MaterialCommunityIcons, Octicons } from '@expo/vector-icons';
-
-import Home from './src/BottomTab/Home';
-import Search from './src/BottomTab/Search';
-import Ticket from './src/BottomTab/Ticket';
-import Profile from './src/BottomTab/Profile';
-import Account from './src/Stack/Account';
-import EditProfile from './src/Stack/EditProfile';
-import Settings from './src/Stack/Settings';
-import MovieScreen from './src/Stack/MovieScreen';
-
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-const screenOptions = {
-    tabBarActiveTintColor: '#E3651D',
-    tabBarHideOnKeyboard: true,
-    tabBarStyle: {
-        backgroundColor: 'black',
-    }
-}
-
-function TabNavigator () {
+const App = () => {
     return(
-        <Tab.Navigator screenOptions={screenOptions}>
-            <Tab.Screen name="Home" component={StackNavigator} options={{
-                headerShown: false,
-                tabBarIcon: ({size, color}) => (
-                    <AntDesign name="home" size={size} color={color} />
-                )
-            }}/>
-            <Tab.Screen name="Search" component={Search} options={{
-                headerShown: false,
-                tabBarIcon: ({size, color}) => (
-                    <AntDesign name="search1" size={size} color={color} />
-                )
-            }}/>
-            <Tab.Screen name="Ticket" component={Ticket} options={{
-                headerShown: false,
-                tabBarIcon: ({size, color}) => (
-                    <MaterialCommunityIcons name="ticket-confirmation-outline" size={size} color={color} />
-                )
-            }}/>
-            <Tab.Screen name="Profile" component={Profile} options={{
-                headerShown: false,
-                tabBarIcon: ({size, color}) => (
-                    <Octicons name="person" size={size} color={color} />
-                )
-            }}/>
-        </Tab.Navigator>
+        <View>
+            <Text>This is a Movixer App.</Text>
+        </View>
     )
 }
 
-function StackNavigator () {
-    return(
-        <Stack.Navigator screenOptions={{
-            headerShown: false
-        }}>
-            <Stack.Screen name="HomeScreen" component={Home} />
-            <Stack.Screen name="Account" component={Account} />
-            <Stack.Screen name="EditProfile" component={EditProfile} />
-            <Stack.Screen name="Settings" component={Settings} />
-            <Stack.Screen name="MovieScreen" component={MovieScreen} />
-        </Stack.Navigator>
-    )
-}
-
-export default function App () {
-    return(
-        <NavigationContainer>
-            <TabNavigator/>
-        </NavigationContainer>
-    )
-}
-
+export default App
