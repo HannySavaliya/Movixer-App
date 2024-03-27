7f675d25f498bc45fbfe061174d45359e1d3213d
HannySavaliya <hannysavaliya1102@gmail.com>
2024-03-27 20:27:20 +0530

    HS | Build Profile Screen UI Code and Movie Screen UI Code

--- App.js
+++ App.js
@@ -9,6 +9,11 @@ import Home from './src/BottomTab/Home';
 import Search from './src/BottomTab/Search';
 import Ticket from './src/BottomTab/Ticket';
 import Profile from './src/BottomTab/Profile';
+import MovieScreen from './src/Stack/MovieScreen';
+import SplashScreen from './src/Stack/SplashScreen';
+import Account from './src/Stack/Account';
+import EditProfile from './src/Stack/EditProfile';
+import Settings from './src/Stack/Settings';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -23,7 +28,7 @@ const screenOptions = {
 function TabNavigator () {
     return(
         <Tab.Navigator screenOptions={screenOptions}>
-            <Tab.Screen name="HomeScreen" component={StackNavigator} options={{
+            <Tab.Screen name="Home" component={StackNavigator} options={{
                 headerShown: false,
                 tabBarIcon: ({size, color}) => (
                     <AntDesign name="home" size={size} color={color} />
@@ -56,7 +61,12 @@ function StackNavigator () {
         <Stack.Navigator screenOptions={{
             headerShown: false
         }}>
-            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="SplashScreen" component={SplashScreen} />
+            <Stack.Screen name="HomeScreen" component={Home} />
+            <Stack.Screen name="MovieScreen" component={MovieScreen} />
+            <Stack.Screen name="Account" component={Account} />
+            <Stack.Screen name="EditProfile" component={EditProfile} />
+            <Stack.Screen name="Settings" component={Settings} />
         </Stack.Navigator>
     )
 }
--- src/BottomTab/Ticket.js
+++ src/BottomTab/Ticket.js
@@ -5,9 +5,10 @@ import { AntDesign } from '@expo/vector-icons';
 const Ticket = ({ navigation }) => {
     return (
         <View style={style.container}>
+            {/* <View style={style.ticket}> */}
             <View style={style.cancelBtn}> 
                 <Pressable onPress={() => navigation.navigate('Home')}>
-                    <AntDesign name="closecircle" size={24} color="darkorange" style={style.icon} />
+                    <AntDesign name="closecircle" size={24} color="#E3651D" style={style.icon} />
                 </Pressable>
                 <Text style={style.title}>My Tickets</Text>
             </View>
@@ -18,12 +19,19 @@ const Ticket = ({ navigation }) => {
                 <Text style={style.infoText}>Time :</Text>
                 <Text style={style.infoText}>Location :</Text>
                 <Text style={style.infoText}>Seat :</Text>
-                <Text style={style.infoText}>Price :</Text>
-                <Pressable style={style.button} onPress={() => navigation.navigate('stack', { screen: 'Payment' })}>
-                    <Text style={style.buttonText} title='Pay' color='darkorange'>Pay</Text>
-                </Pressable>
+                <View style={style.break}>
+                    <Text style={style.round}></Text>
+                    <Text style={style.line}>----------------------------------</Text>
+                    <Text style={style.round}></Text>
+                </View>
+                <View style={style.price}>
+                    <Text style={style.priceText}>Price</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('Payment')}>
+                        <Text style={style.buttonText}>Pay</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-
+            {/* </View> */}
         </View>
     );
 };
@@ -32,13 +40,13 @@ const style = StyleSheet.create({
     container: {
         backgroundColor: 'black',
         height: '100%',
-        alignItems: 'center'
-        
+        alignItems: 'center' ,
+        justifyContent: 'center' 
     },
     cancelBtn: {
         flexDirection: 'row',
         marginLeft: -70,
-        justifyContent: 'center'
+        marginTop: -300
     },
     icon: {
         marginTop: 68,
@@ -53,42 +61,64 @@ const style = StyleSheet.create({
         fontWeight: 'bold'
     },
     ticket: {
+        backgroundColor: '#E3651D',
         marginTop: 20,
-        padding: 30,
-        height : 300,
-        width : 320,
-        paddingVertical: 20,
-        borderRadius: 10,
+        paddingVertical: 30,
+        paddingHorizontal: 30,
+        borderRadius: 25,
+        width: 350,
         borderWidth: 1,
-        borderColor: 'grey',
     },
     movieTitle: {
         textAlign: 'center',
         color: 'white',
-        fontSize: 20,
+        fontSize: 22,
         fontWeight: 'bold',
         marginBottom: 10,
     },
     infoText: {
         color: 'white',
-        fontSize: 16,
+        fontSize: 18,
         marginBottom: 7,
     },
-    button: {
-        backgroundColor: 'darkorange',
-        padding: 5,
-        borderRadius: 25,
-        marginTop: 10, 
-        marginHorizontal: 50 ,
-        marginBottom: 20
+    priceText: {
+        color: 'white',
+        fontSize: 23,
     },
-    buttonText: {
-        fontSize: 18,
-        color: '#ffffff',
-        fontWeight: 'bold',
+    break: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: -78,
+        marginTop: 30
+    },
+    line: {
+        fontSize: 30,
+        color: 'white',
         textAlign: 'center'
     },
-    
+    round: {
+        width: 80,
+        height: 80,
+        backgroundColor: 'black',
+        borderRadius: 50
+    },
+    price: {
+        flexDirection: 'row' ,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        marginTop: 10
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#ffffff',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        backgroundColor: 'black',
+        opacity: 0.8,
+        padding: 7,
+        paddingHorizontal: 60,
+        borderRadius: 20
+    },
 });
     
 export default Ticket;
